3580dd69fe55cb7b43a526905b7fc7da3b45f7e0
takumi-pro <65642316+takumi-pro@users.noreply.github.com>
2021-05-08 21:34:24 +0900

    機能実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,67 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  // console.log("first");
+  const [todoText, settodoText] = useState("");
   const [imcompleteTodos, setimcompleteTodos] = useState(["hi", "hii"]);
   const [completeTodos, setcompleteTodos] = useState(["complete"]);
+
+  //inputのchangeイベント
+  const onChangeTodoText = (e) => settodoText(e.target.value);
+
+  //タスク追加処理
+  const onClickAdd = () => {
+    const newTodos = [...imcompleteTodos, todoText];
+    setimcompleteTodos(newTodos);
+    settodoText("");
+    // alert(todoText);
+  };
+
+  //タスク削除処理
+  const onClickDelete = (index) => {
+    deleteCommon(index, imcompleteTodos);
+  };
+
+  //タスク完了処理
+  const onClickComplete = (index) => {
+    deleteCommon(index, imcompleteTodos);
+
+    const newCompleteTodos = [...completeTodos, imcompleteTodos[index]];
+    setcompleteTodos(newCompleteTodos);
+  };
+
+  //戻る処理
+  const onClickBack = (index) => {
+    deleteCommon(index, completeTodos, true);
+
+    const newImcompleteTodos = [...imcompleteTodos, completeTodos[index]];
+    setimcompleteTodos(newImcompleteTodos);
+  };
+
+  //削除処理（共通）
+  const deleteCommon = (index, targetTodos, completeFlg = false) => {
+    const newTodos = [...targetTodos];
+    newTodos.splice(index, 1);
+
+    completeFlg ? setcompleteTodos(newTodos) : setimcompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="taskadd-area">
-        <input type="text" />
-        <button>追加</button>
+        <input type="text" value={todoText} onChange={onChangeTodoText} />
+        <button onClick={onClickAdd}>追加</button>
       </div>
 
       <div className="imcomplete-area">
         <p className="title">未完了のタスク</p>
         <ul>
-          {imcompleteTodos.map((todo) => {
+          {imcompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="flex">
                 <li>{todo}</li>
-                <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
@@ -29,11 +72,11 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了タスク</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <div className="flex">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={() => onClickBack(index)}>戻す</button>
               </div>
             );
           })}
